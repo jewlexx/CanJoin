e2ad333d84b0ffa9e6c7f8a0a733efd339f56832
Juliette Cordor <grassojames5@gmail.com>
2023-02-01 18:00:31 +1100

    updates and fixes

--- src/main/java/com/jewelexx/canjoin/Canjoin.java
+++ src/main/java/com/jewelexx/canjoin/Canjoin.java
@@ -10,6 +10,9 @@ import org.yaml.snakeyaml.Yaml;
 
 import net.kyori.adventure.text.Component;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -40,10 +43,23 @@ class CanJoinEvents implements Listener {
 
 public final class Canjoin extends JavaPlugin {
     int maxTime;
-    HashMap<String, Integer> playerTimes = new HashMap<String, Integer>();
+    HashMap<String, Integer> playerTimes = new HashMap<>();
 
     @Override
     public void onEnable() {
+        try {
+            File timesFile = new File("player-times.yml");
+            FileInputStream fileInputStream = new FileInputStream(timesFile);
+            Yaml yamlFile = new Yaml();
+
+            HashMap<String, Integer> map = yamlFile.load(fileInputStream);
+
+            if (getCurrentDate() == map.get("date")) {
+                playerTimes = map;
+            }
+        } catch (FileNotFoundException ignore) {
+        }
+
         saveDefaultConfig();
         maxTime = getConfig().getInt("max-time");
 
@@ -53,6 +69,11 @@ public final class Canjoin extends JavaPlugin {
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
+                // Reset the player times on a new day
+                if (getCurrentDate() != playerTimes.get("date")) {
+                    playerTimes = new HashMap<>();
+                }
+
                 Bukkit.getOnlinePlayers().forEach((player) -> updatePlayerTime(player));
             }
         }, 20L, 20L);
@@ -74,12 +95,16 @@ public final class Canjoin extends JavaPlugin {
         getLogger().info("Player " + player.getName() + " has been online for " + time + " seconds");
     }
 
-    public void dumpPlayerTimes() throws IOException {
+    public Integer getCurrentDate() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
         LocalDateTime now = LocalDateTime.now();
 
+        return Integer.parseInt(dtf.format(now));
+    }
+
+    public void dumpPlayerTimes() throws IOException {
         HashMap<String, Integer> playerTimes = this.playerTimes;
-        playerTimes.put("date", Integer.parseInt(dtf.format(now)));
+        playerTimes.put("date", getCurrentDate());
 
         Yaml yamlFile = new Yaml();
         FileWriter file = new FileWriter("player-times.yml", Charset.defaultCharset());
